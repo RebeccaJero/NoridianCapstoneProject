208e4b479019488cacf75fc0fc09a14c570b5991
RebeccaJero <rebecca.chepkirwok@ndsu.edu>
2021-02-24 18:46:00 -0600

    updateddb

---
+++ TrackTaskItemsDb/Controllers/StrategicPillarsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrackTaskItemsDb.Models;
+
+namespace TrackTaskItemsDb.Controllers
+{
+    public class StrategicPillarsController : Controller
+    {
+        private TrackTasksEntities db = new TrackTasksEntities();
+
+        // GET: StrategicPillars
+        public ActionResult Index()
+        {
+            return View(db.StrategicPillars.ToList());
+        }
+
+        // GET: StrategicPillars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StrategicPillar strategicPillar = db.StrategicPillars.Find(id);
+            if (strategicPillar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(strategicPillar);
+        }
+
+        // GET: StrategicPillars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StrategicPillars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Goal")] StrategicPillar strategicPillar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StrategicPillars.Add(strategicPillar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(strategicPillar);
+        }
+
+        // GET: StrategicPillars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StrategicPillar strategicPillar = db.StrategicPillars.Find(id);
+            if (strategicPillar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(strategicPillar);
+        }
+
+        // POST: StrategicPillars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Goal")] StrategicPillar strategicPillar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(strategicPillar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(strategicPillar);
+        }
+
+        // GET: StrategicPillars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StrategicPillar strategicPillar = db.StrategicPillars.Find(id);
+            if (strategicPillar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(strategicPillar);
+        }
+
+        // POST: StrategicPillars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StrategicPillar strategicPillar = db.StrategicPillars.Find(id);
+            db.StrategicPillars.Remove(strategicPillar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TrackTaskItemsDb/Controllers/TaskItemsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrackTaskItemsDb.Models;
+
+namespace TrackTaskItemsDb.Controllers
+{
+    public class TaskItemsController : Controller
+    {
+        private TrackTasksEntities db = new TrackTasksEntities();
+
+        // GET: TaskItems
+        public ActionResult Index()
+        {
+            var taskItems = db.TaskItems.Include(t => t.Status1);
+            return View(taskItems.ToList());
+        }
+
+        // GET: TaskItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskItem taskItem = db.TaskItems.Find(id);
+            if (taskItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskItem);
+        }
+
+        // GET: TaskItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc");
+            ViewBag.Department = new SelectList(db.Departments, "Id", "Department_Name");
+            return View();
+        }
+
+        // POST: TaskItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Status,IsMandate,MandateComment,Action,IT_Project_Number,LastModifiedDate,CreatedDate,CompletedDate,StartDate")] TaskItem taskItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaskItems.Add(taskItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc", taskItem.Status);
+            return View(taskItem);
+        }
+
+        // GET: TaskItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskItem taskItem = db.TaskItems.Find(id);
+            if (taskItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc", taskItem.Status);
+            return View(taskItem);
+        }
+
+        // POST: TaskItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Status,IsMandate,MandateComment,Action,IT_Project_Number,LastModifiedDate,CreatedDate,CompletedDate,StartDate")] TaskItem taskItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taskItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc", taskItem.Status);
+            return View(taskItem);
+        }
+
+        // GET: TaskItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskItem taskItem = db.TaskItems.Find(id);
+            if (taskItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskItem);
+        }
+
+        // POST: TaskItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaskItem taskItem = db.TaskItems.Find(id);
+            db.TaskItems.Remove(taskItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
