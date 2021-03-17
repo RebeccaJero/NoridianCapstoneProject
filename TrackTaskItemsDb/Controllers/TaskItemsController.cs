216a2ae3386f7a7b5ee2a162467d0a8ecd1ca9bf
RebeccaJero <rebecca.chepkirwok@ndsu.edu>
2021-03-17 16:07:47 -0500

    commit changes for create item

--- TrackTaskItemsDb/Controllers/TaskItemsController.cs
+++ TrackTaskItemsDb/Controllers/TaskItemsController.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TrackTaskItemsDb.Models;
+using TrackTaskItemsDb.Validators;
 
 namespace TrackTaskItemsDb.Controllers
 {
-    [System.Web.Mvc.Authorize]
+    //[System.Web.Mvc.Authorize]
     public class TaskItemsController : Controller
     {
         private TrackTasksEntities db = new TrackTasksEntities();
+        private IValidator<TaskItem> taskItemValidator;
+
+        public TaskItemsController()
+        {
+            this.taskItemValidator = new TaskItemValidator();
+        }
 
         // GET: TaskItems
         public ActionResult Index()
@@ -44,28 +52,200 @@ namespace TrackTaskItemsDb.Controllers
             ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc");
             ViewBag.Department = new SelectList(db.Departments, "Id", "Department_Name");
             ViewBag.Pillars = new SelectList(db.StrategicPillars, "Id", "StrategicPillar1");
-          
-          
-            ViewBag.Quarters = new SelectList(db.Quarters, "Id", "Quarter_Desc");
+            ViewBag.StartQuarters = new SelectList(db.Quarters.OrderBy(s=>s.StartDate), "StartDate", "Quarter_Desc", "Id");
+            ViewBag.EndQuarters = new SelectList(db.Quarters.OrderBy(s => s.StartDate), "StartDate", "Id", "Quarter_Desc");
+           
             return View();
         }
 
-        // POST: TaskItems/Create
+        public ActionResult CreateWithJson()
+        {
+
+            ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc");
+            ViewBag.Department = new SelectList(db.Departments, "Id", "Department_Name");
+            ViewBag.Pillars = new SelectList(db.StrategicPillars, "Id", "StrategicPillar1");
+            ViewBag.StartQuarters = new SelectList(db.Quarters.OrderBy(s => s.StartDate), "StartDate", "Quarter_Desc", "Id");
+            return View();
+        }
+
+
+
+        // POST: TaskItems/CreateWithJson
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Status,IsMandate,MandateComment,Action,IT_Project_Number,LastModifiedDate,CreatedDate,CompletedDate,StartDate")] TaskItem taskItem)
+        public ActionResult CreateWithJson(TaskItem taskItem)
         {
-            if (ModelState.IsValid)
+            
+            //server side validation
+            var isInvalid = this.taskItemValidator.TryInvalidate(taskItem, out string errorMessage);
+
+
+            if (isInvalid)
+            {
+                ModelState.AddModelError("CompletedDate", errorMessage);
+                ModelState.AddModelError("StartDate", errorMessage);
+                ModelState.AddModelError("StartQuarter", errorMessage);
+
+                ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc");
+                ViewBag.Department = new SelectList(db.Departments, "Id", "Department_Name");
+                ViewBag.Pillars = new SelectList(db.StrategicPillars, "Id", "StrategicPillar1");
+                ViewBag.StartQuarters = new SelectList(db.Quarters.OrderBy(s => s.StartDate), "StartDate", "Quarter_Desc", "Id");
+                return View(taskItem);
+            }
+
+            //Get current userId
+            var user = User.Identity.Name;
+            var userId = db.Users.Where(u => u.UserIdentifier == user).Select(id => id.Id).FirstOrDefault();
+            var itemDepartmentCode = "";
+
+            //Insert into TaskItem database
+            taskItem.IT_Project_Number = string.IsNullOrEmpty(taskItem.IT_Project_Number) ? "N/A" : taskItem.IT_Project_Number;
+            taskItem.CreatedDate = DateTime.Now;
+            taskItem.LastModifiedDate = DateTime.Now;
+            try
             {
                 db.TaskItems.Add(taskItem);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+
+                var quarterItem = taskItem.QuarterItems.FirstOrDefault();
+                quarterItem.TaskItemId = taskItem.Id;
+                quarterItem.isOriginal = true;
+                quarterItem.CreatedDate = DateTime.Now;
+                db.QuarterItems.Add(quarterItem);
+                db.SaveChanges();
+
+                //get code for main department
+                var depNotImpacted = taskItem.ItemDepartments.Where(d => d.IsImpacted == false).FirstOrDefault();
+                itemDepartmentCode = db.Departments.Where(d => d.Id == depNotImpacted.DepartmentId).Select(id => id.Code).FirstOrDefault();
+
+                //insert ItemDepartment in database
+                foreach (var itemDepartment in taskItem.ItemDepartments)
+                {
+                    itemDepartment.TaskItemId = taskItem.Id;
+                    itemDepartment.UserId = userId;
+                    itemDepartment.ItemNumber = itemDepartmentCode + "-" + taskItem.Id;
+                    db.ItemDepartments.Add(itemDepartment);
+                    db.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+                var dbError=ex.Message;
             }
 
-            ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc", taskItem.Status);
-            return View(taskItem);
+            return RedirectToAction("Index");
+        }
+        
+        
+        // POST: TaskItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+      // [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IsMandate,MandateComment,StartQuarter,EndQuarter,Action,IT_Project_Number,LastModifiedDate,CreatedDate,CompletedDate,StartDate,OutCome,Department,ImpactedDepartments,Status,StrategicPillarId,OperationalBudgetImplications")] TaskItem taskItem)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.StartQuarters = new SelectList(db.Quarters.OrderBy(s => s.StartDate), "StartDate", "Quarter_Desc");
+                ViewBag.EndQuarters = new SelectList(db.Quarters.OrderBy(s => s.StartDate), "StartDate", "Quarter_Desc");
+                ViewBag.Status = new SelectList(db.Status, "Id", "Status_Desc");
+                ViewBag.Department = new SelectList(db.Departments, "Id", "Department_Name");
+                ViewBag.Pillars = new SelectList(db.StrategicPillars, "Id", "StrategicPillar1");
+                return View(taskItem);
+            }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        var newTaskItem = new TaskItem();
+        //        var quarterItems = new QuarterItem();
+        //        var itemDepartment = new ItemDepartment();
+                
+        //        var user = User.Identity.Name;
+        //        var userId = db.Users.Where(u => u.UserIdentifier == user).Select(id => id.Id).FirstOrDefault();
+        //        var itemDepartmentNum = db.Departments.Where(d => d.Id == taskItem.Department).Select(id => id.Code).FirstOrDefault();
+        //        //var starQuarterId = db.Quarters.Where(q => q.StartDate == taskItem.StartQuarter).Select(id => id.Id).FirstOrDefault();
+        //       // var endQuarterId = db.Quarters.Where(q => q.StartDate == taskItem.EndQuarter).Select(id => id.Id).FirstOrDefault();
+
+ 
+
+        ////Insert into TaskItem database
+        //        newTaskItem.Status = taskItem.Status;
+        //        newTaskItem.IsMandate = taskItem.IsMandate;
+        //        newTaskItem.MandateComment = taskItem.MandateComment;
+        //        newTaskItem.Action = taskItem.Action;
+        //        newTaskItem.IT_Project_Number = string.IsNullOrEmpty(taskItem.IT_Project_Number) ? "N/A" : taskItem.IT_Project_Number;
+        //        newTaskItem.CreatedDate = DateTime.Now;
+        //        newTaskItem.LastModifiedDate = DateTime.Now;
+        //        newTaskItem.StartDate = taskItem.StartDate;
+        //        newTaskItem.CompletedDate = taskItem.CompletedDate;
+        //        newTaskItem.OperationalBudgetImplications = taskItem.OperationalBudgetImplications;
+        //        newTaskItem.Outcome = taskItem.Outcome;
+        //        newTaskItem.StrategicPillarId = taskItem.StrategicPillarId;
+        //        db.TaskItems.Add(newTaskItem);
+                 
+        //        try
+        //        {
+        //            // Your code...
+        //            // Could also be before try if you know the exception occurs in SaveChanges
+
+        //            db.SaveChanges();
+        //        }
+        //        catch (DbEntityValidationException e)
+        //        {
+        //            foreach (var eve in e.EntityValidationErrors)
+        //            {
+        //                var msg =("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+        //                    eve.Entry.Entity.GetType().Name, eve.Entry.State);
+        //                foreach (var ve in eve.ValidationErrors)
+        //                {
+        //                   var info=("- Property: \"{0}\", Error: \"{1}\"",
+        //                        ve.PropertyName, ve.ErrorMessage);
+        //                }
+        //            }
+        //            throw;
+        //        }
+        //        //Insert into ItemDepartment department not impacted
+        //        itemDepartment.TaskItemId = taskItem.Id;
+        //        itemDepartment.IsImpacted = false;
+        //        itemDepartment.DepartmentId = taskItem.Department;
+        //        itemDepartment.UserId = userId;
+        //        itemDepartment.ItemNumber = itemDepartmentNum + "-" + taskItem.Id;
+        //        db.ItemDepartments.Add(itemDepartment);
+        //        db.SaveChanges();
+
+
+        //      //Insert into ItemDepartment departments impacted
+        //        foreach (var depId in taskItem.ImpactedDepartments)
+        //        {
+        //           // var itemId = db.Departments.Where(d => d.Id == depId).Select(id => id.Department_Name).FirstOrDefault();
+        //            itemDepartment.TaskItemId = taskItem.Id;
+        //            itemDepartment.IsImpacted = true;
+        //            itemDepartment.DepartmentId =depId;
+        //            itemDepartment.UserId = userId;
+        //            itemDepartment.ItemNumber = itemDepartmentNum + "-" + taskItem.Id;
+        //            db.ItemDepartments.Add(itemDepartment);
+        //            db.SaveChanges();
+        //        }
+
+
+        //        //Insert into QuarterItems
+        //        quarterItems.TaskItemId = taskItem.Id;
+        //        quarterItems.isOriginal = true;
+        //        quarterItems.isUpdated = false;
+        //       // quarterItems.StartQuarterId = starQuarterId;
+        //       // quarterItems.EndQuarterId = endQuarterId;
+        //        quarterItems.CreatedDate = DateTime.Now;
+        //       db.QuarterItems.Add(quarterItems);
+        //       db.SaveChanges();
+
+        //        return RedirectToAction("Index");
+        //    }
+
+            return View();
         }
 
         // GET: TaskItems/Edit/5
---
+++ TrackTaskItemsDb/Scripts/CustomValidators.js
@@ -0,0 +1,202 @@
+﻿
+
+
+(function ($) {
+
+    $.validator.addMethod("comparequarters",
+        function (value, element, params) {
+
+           
+                return new Date(value) > new Date($(params).val());
+  
+        });
+
+    $.validator.addMethod("comparedates",
+        function (value, element, params) {
+
+            if ($(params).val()== null || $(params).val() == "") {
+                return true;
+            }
+            if (params != "") {
+                return new Date(value) < new Date($(params).val());
+            }
+          
+
+        });
+
+
+    $.validator.addMethod("completeddatestatus",
+        function (value, element, params) {
+
+            if (value == null || value == "") {
+                return true;
+            }
+            if (value != "") {
+                return parseInt($(params).val()) == 6;
+            }
+
+
+        });
+
+    $.validator.addMethod("statuscompleted",
+        function (value, element, params) {
+          
+            if ($(params).val() == "") {
+                return value != "6";
+            }
+            return true;
+        });
+
+}(jQuery));
+
+$(document).ready(function () {
+
+    $('#ImpactedDep').multiselect({
+        includeSelectAllOption: true
+    });
+
+    $("#StartDate").datepicker();
+
+    $("#CompletedDate").datepicker();
+
+
+    $('#myform').validate({ // initialize the plugin
+        rules: {
+            Action: {
+                required: true,
+                maxlength: 250
+            },
+            Status: {
+                required: true,
+                statuscompleted: CompletedDate
+
+            },
+            StartDate: {
+                required: true,
+                comparedates: CompletedDate
+
+            },
+            CompletedDate: {
+                
+                completeddatestatus: Status
+
+            },
+            StrategicPillarId: {
+                required: true
+
+            },
+            Outcome: {
+                required: true,
+                maxlength: 500
+            },
+            EndQuarter: { comparequarters: StartQuarter },
+            MandateComment: {
+
+                maxlength: 25
+            }
+        },
+        messages: {
+            Action: {
+                required: "Action is required!",
+                maxlength: "250 maximum characters!"
+
+            },
+            Status: {
+                required: "Status is required!",
+                statuscompleted: "Please fill in the Completed Date"
+            },
+            StartDate: {
+                required: "Start Date is required!",
+                comparedates: "Completed Date should be greater than Start Date"
+
+            },
+            CompletedDate: {
+
+                completeddatestatus: "Status has to be Complete"
+
+            },
+            StrategicPillarId: {
+                required: "Strategic Pillar is required!"
+
+            },
+            EndQuarter: {
+                comparequarters: "End Quarter should be greater than Start Quarter"
+            },
+
+            Outcome: {
+                required: "Outcome is required!",
+                maxlength: "500 maximum characters!"
+
+            },
+            MandateComment: {
+                maxlength: "25 maximum characters!"
+            }
+
+        }
+
+
+    });
+
+    $("#myform").on('submit', function (e) {
+        if ($("#myform").valid()) {
+            e.preventDefault();
+            //create task item objects
+            var taskItem = {};
+            var primaryDepartment = {};
+            var quarterItem = {};
+            var impactedDepartments = new Array();
+            for (var option of document.getElementById('ImpactedDep').options) {
+                if (option.selected) {
+                    var impactedDep = {};
+                    impactedDep.DepartmentId = option.value;
+                    impactedDep.IsImpacted = true;
+                    impactedDepartments.push(impactedDep);
+                    
+                }
+            }
+
+          
+            var primaryDep = $("#Department option:selected").val();
+            var token = $('input[name="__RequestVerificationToken"]').val();
+            primaryDepartment.DepartmentId = primaryDep;
+            primaryDepartment.IsImpacted = false;
+            impactedDepartments.push(primaryDepartment);
+
+            quarterItem.StartQuarterId = $("#StartQuarter option:selected").attr("quarterId");
+            quarterItem.EndQuarterId = $("#EndQuarter option:selected").attr("quarterId");
+            taskItem.Outcome = $("#Outcome").val();
+            taskItem.Action = $("#Action").val();
+            taskItem.MandateComment = $("#MandateComment").val();
+            taskItem.IsMandate = $("#isMandate").val();
+            taskItem.IT_Project_Number = $("#IT_Project_Number").val();
+            taskItem.OperationalBudgetImplications = $("#OperationalBudgetImplications").val();
+            taskItem.Department = $("#Department").val();
+            taskItem.ItemDepartments = impactedDepartments;
+            taskItem.Status = $("#Status").val();
+            taskItem.StartDate = $("#StartDate").val();
+            taskItem.CompletedDate = $("#CompletedDate").val();
+            taskItem.StrategicPillarId = $("#StrategicPillarId").val();
+            taskItem.QuarterItems = [quarterItem];
+
+            $.ajax({
+                type: "POST",
+                url: "/TaskItems/CreateWithJson",
+                contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+                data: {
+                    __RequestVerificationToken: token,
+                    taskItem:taskItem,
+                },
+                dataType: 'JSON',
+                //data: JSON.stringify(taskItem),
+                success: function (data) { alert(data); },
+                failure: function (errMsg) {
+                    alert(errMsg);
+                }
+
+            });
+
+        }
+    });
+});
+
+
